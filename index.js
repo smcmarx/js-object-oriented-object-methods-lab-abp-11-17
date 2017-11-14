7231965eaf6398d346ae927843cf842f6e8cf7f9
Stephanie McSpedon-Marx <s.mcspedon@gmail.com>
2017-11-14 16:24:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,54 +22,10 @@ function BoardMember(name, homeState, training){
   }
 
   BoardMember.prototype.sayHi = function(){
-    return "Hi, my name is ${this.name}, I am from ${this.homeState}, and I was trained in ${this.training}."
+    return `Hi, my name is ${this.name}, I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 
-polishedBoardMember.sayHi()
 
 
 
 
-/*
-  veto(){`No, I must disagree`}
-  approve(){`You can do that!`}
-  doCharity(){`I like to help people.`}
-  releasePressStatement(){`You will see great things from Scuber.`}
-  sayHi(){}
-
-}
-
-class Ceo extends BoardMember{
-
-}
-/*
-function BoardMember(name, homeState, training) {
-  this.name = name
-  this.homeState = homeState
-  this.training = training
-
-  }
-  BoardMember.prototype.veto = function(){
-    return "No, I must disagree"
-  }
-
-  BoardMember.prototype.approve = function(){
-    return "You can do that!"
-  }
-
-  BoardMember.prototype.doCharity = function(){
-    return "I like to help people."
-  }
-
-  BoardMember.prototype.releasePressStatement = function(){
-    return "You will see great things from Scuber."
-  }
-
-  BoardMember.prototype.sayHi = function(){
-    return "Hi, my name is ${this.name}, I am from ${this.homeState}, and I was trained in ${this.training}."
-  }
-
-*/
-
-
-let polishedBoardMember = new BoardMember("Mr.Polished","New York", "Law" )
